779ea7abbc27e4d3bb31b6ea0929f23b12a0b781
Tami Bar <fire17@gmail.com>
2021-08-10 12:54:33 +0300

    prep v3.2.2_4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '3.2.1.2'
+__version__ = '3.2.2_4'
 
 here = path.abspath(path.dirname(__file__))
 
